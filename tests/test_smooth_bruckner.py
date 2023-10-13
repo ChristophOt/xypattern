44f6544edc5f06f0bd526bf8b94f50855a2d3788
Clemens Prescher <clemens.prescher@gmail.com>
2023-10-13 10:06:48 +0200

    further loosening smooth bruckner comparison, since there is an order of magnitude larger error on CI container

--- tests/test_smooth_bruckner.py
+++ tests/test_smooth_bruckner.py
@@ -11,4 +11,4 @@ def test_both_smooth_bruckner_implementations_give_same_result():
     bkg1 = smooth_bruckner(pattern.y, 3, 50)
     bkg2 = smooth_bruckner_py(pattern.y, 3, 50)
 
-    assert np.sum(bkg1 - bkg2) == approx(0.0, abs=1e-11)
+    assert np.sum(bkg1 - bkg2) == approx(0.0, abs=1e-8)
